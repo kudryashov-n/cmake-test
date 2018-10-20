f85b37dfc45ee504c0e64599ff21b7b332630efb
katy1819 <katerinka.fedotova1819@gmail.com>
2018-10-20 14:01:40 +0300

    fsdf

--- code.cpp
+++ code.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int square(int gh)
+{
+    return gh*gh;
+}
+
+int cube(int gh)
+{
+    return gh*gh*gh;
+}
+
+int min2us(int gh)
+{
+    return -gh;
+}
+
 int main(int argc,char** argv)
 {
-    cout << argv[1] << argv[2];
+    cout << square(3) << cube(3) << min2us(3);
     return 0;
 }
